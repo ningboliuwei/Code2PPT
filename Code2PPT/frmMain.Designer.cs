f99d9f511c6e1344d6b119c34f4bdbca7061f990
ningboliuwei <liuwei@nbu.edu.cn>
2014-09-30 10:41:21 +0800

    基本成型，第二个发布版本（修正了 \\pard 的问题）

---
+++ Code2PPT/frmMain.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace Code2PPT
+{
+	partial class frmMain
+	{
+		/// <summary>
+		/// 必需的设计器变量。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 清理所有正在使用的资源。
+		/// </summary>
+		/// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows 窗体设计器生成的代码
+
+		/// <summary>
+		/// 设计器支持所需的方法 - 不要
+		/// 使用代码编辑器修改此方法的内容。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+			this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+			this.label1 = new System.Windows.Forms.Label();
+			this.cmbFontName = new System.Windows.Forms.ComboBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.cmbFontSize = new System.Windows.Forms.ComboBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.cmbTabSpaces = new System.Windows.Forms.ComboBox();
+			this.chkAlwaysOnTop = new System.Windows.Forms.CheckBox();
+			this.btnHelp = new System.Windows.Forms.Button();
+			this.rtxCode = new System.Windows.Forms.RichTextBox();
+			this.tableLayoutPanel1.SuspendLayout();
+			this.flowLayoutPanel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// tableLayoutPanel1
+			// 
+			this.tableLayoutPanel1.ColumnCount = 1;
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 0, 0);
+			this.tableLayoutPanel1.Controls.Add(this.rtxCode, 0, 1);
+			this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+			this.tableLayoutPanel1.RowCount = 2;
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 35F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(684, 461);
+			this.tableLayoutPanel1.TabIndex = 0;
+			// 
+			// flowLayoutPanel1
+			// 
+			this.flowLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.flowLayoutPanel1.Controls.Add(this.label1);
+			this.flowLayoutPanel1.Controls.Add(this.cmbFontName);
+			this.flowLayoutPanel1.Controls.Add(this.label2);
+			this.flowLayoutPanel1.Controls.Add(this.cmbFontSize);
+			this.flowLayoutPanel1.Controls.Add(this.label3);
+			this.flowLayoutPanel1.Controls.Add(this.cmbTabSpaces);
+			this.flowLayoutPanel1.Controls.Add(this.chkAlwaysOnTop);
+			this.flowLayoutPanel1.Controls.Add(this.btnHelp);
+			this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+			this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+			this.flowLayoutPanel1.Size = new System.Drawing.Size(678, 29);
+			this.flowLayoutPanel1.TabIndex = 3;
+			// 
+			// label1
+			// 
+			this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(3, 7);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(32, 17);
+			this.label1.TabIndex = 4;
+			this.label1.Text = "字体";
+			// 
+			// cmbFontName
+			// 
+			this.cmbFontName.Anchor = System.Windows.Forms.AnchorStyles.None;
+			this.cmbFontName.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.cmbFontName.FormattingEnabled = true;
+			this.cmbFontName.Location = new System.Drawing.Point(41, 3);
+			this.cmbFontName.Name = "cmbFontName";
+			this.cmbFontName.Size = new System.Drawing.Size(121, 25);
+			this.cmbFontName.TabIndex = 0;
+			this.cmbFontName.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+			// 
+			// label2
+			// 
+			this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(168, 7);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(32, 17);
+			this.label2.TabIndex = 5;
+			this.label2.Text = "字号";
+			// 
+			// cmbFontSize
+			// 
+			this.cmbFontSize.Anchor = System.Windows.Forms.AnchorStyles.None;
+			this.cmbFontSize.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.cmbFontSize.FormattingEnabled = true;
+			this.cmbFontSize.Location = new System.Drawing.Point(206, 3);
+			this.cmbFontSize.Name = "cmbFontSize";
+			this.cmbFontSize.Size = new System.Drawing.Size(121, 25);
+			this.cmbFontSize.TabIndex = 1;
+			this.cmbFontSize.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+			// 
+			// label3
+			// 
+			this.label3.Anchor = System.Windows.Forms.AnchorStyles.None;
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(333, 7);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(66, 17);
+			this.label3.TabIndex = 6;
+			this.label3.Text = "Tab空格数";
+			// 
+			// cmbTabSpaces
+			// 
+			this.cmbTabSpaces.Anchor = System.Windows.Forms.AnchorStyles.None;
+			this.cmbTabSpaces.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.cmbTabSpaces.FormattingEnabled = true;
+			this.cmbTabSpaces.Location = new System.Drawing.Point(405, 3);
+			this.cmbTabSpaces.Name = "cmbTabSpaces";
+			this.cmbTabSpaces.Size = new System.Drawing.Size(130, 25);
+			this.cmbTabSpaces.TabIndex = 7;
+			// 
+			// chkAlwaysOnTop
+			// 
+			this.chkAlwaysOnTop.Anchor = System.Windows.Forms.AnchorStyles.None;
+			this.chkAlwaysOnTop.AutoSize = true;
+			this.chkAlwaysOnTop.Checked = true;
+			this.chkAlwaysOnTop.CheckState = System.Windows.Forms.CheckState.Checked;
+			this.chkAlwaysOnTop.Location = new System.Drawing.Point(541, 5);
+			this.chkAlwaysOnTop.Name = "chkAlwaysOnTop";
+			this.chkAlwaysOnTop.Size = new System.Drawing.Size(75, 21);
+			this.chkAlwaysOnTop.TabIndex = 3;
+			this.chkAlwaysOnTop.Text = "始终置顶";
+			this.chkAlwaysOnTop.UseVisualStyleBackColor = true;
+			this.chkAlwaysOnTop.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+			// 
+			// btnHelp
+			// 
+			this.btnHelp.Anchor = System.Windows.Forms.AnchorStyles.None;
+			this.btnHelp.Location = new System.Drawing.Point(622, 5);
+			this.btnHelp.Name = "btnHelp";
+			this.btnHelp.Size = new System.Drawing.Size(20, 20);
+			this.btnHelp.TabIndex = 8;
+			this.btnHelp.Text = "?";
+			this.btnHelp.UseVisualStyleBackColor = true;
+			this.btnHelp.Click += new System.EventHandler(this.btnHelp_Click);
+			// 
+			// rtxCode
+			// 
+			this.rtxCode.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.rtxCode.Location = new System.Drawing.Point(3, 38);
+			this.rtxCode.Name = "rtxCode";
+			this.rtxCode.Size = new System.Drawing.Size(678, 420);
+			this.rtxCode.TabIndex = 4;
+			this.rtxCode.Text = "";
+			this.rtxCode.KeyDown += new System.Windows.Forms.KeyEventHandler(this.rtxCode_KeyDown);
+			// 
+			// frmMain
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(684, 461);
+			this.Controls.Add(this.tableLayoutPanel1);
+			this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MinimumSize = new System.Drawing.Size(700, 300);
+			this.Name = "frmMain";
+			this.Text = "Code 2 PPT";
+			this.TopMost = true;
+			this.Load += new System.EventHandler(this.frmMain_Load);
+			this.tableLayoutPanel1.ResumeLayout(false);
+			this.flowLayoutPanel1.ResumeLayout(false);
+			this.flowLayoutPanel1.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+		private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+		private System.Windows.Forms.ComboBox cmbFontName;
+		private System.Windows.Forms.ComboBox cmbFontSize;
+		private System.Windows.Forms.CheckBox chkAlwaysOnTop;
+		private System.Windows.Forms.RichTextBox rtxCode;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.ComboBox cmbTabSpaces;
+		private System.Windows.Forms.Button btnHelp;
+
+
+	}
+}
+
---
+++ Code2PPT/frmMain.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Code2PPT
+{
+	public partial class frmMain : Form
+	{
+		public frmMain()
+		{
+			InitializeComponent();
+		}
+
+		private void frmMain_Load(object sender, EventArgs e)
+		{
+			//字体下拉菜单
+			InstalledFontCollection installedFontCollection = new InstalledFontCollection();
+			foreach (FontFamily fontFamily in installedFontCollection.Families)
+			{
+				cmbFontName.Items.Add(fontFamily.Name);
+			}
+			cmbFontName.Text = "Consolas";//默认选中Consolas字体
+			//
+
+
+			//字号下拉菜单
+			for (int i = 6; i < 60; i++)
+			{
+				cmbFontSize.Items.Add(i);
+				cmbFontSize.Text = "10";//默认选中10号字
+			}
+			//
+
+			//Tab空格下拉菜单
+			cmbTabSpaces.Items.Add("不将Tab替换为空格");
+			for (int i = 1; i <= 16; i++)
+			{
+				cmbTabSpaces.Items.Add(i);
+				cmbTabSpaces.Text = "4";//默认一个Tab相当于4个空格
+			}
+			//
+
+
+
+		}
+
+
+public void ChangeFont(string fontName, int fontSize)
+{
+	rtxCode.SelectAll();
+	rtxCode.SelectionFont = new Font(fontName, fontSize);
+	rtxCode.Select(0, 0);
+}
+
+		//将Enter硬回车（段落）替换为软回车（Shift+Enter）
+		public string GetChangeEnter2ShfitEnterRtf(string oldRtf)
+		{
+			return oldRtf.Replace("\\pard", "\\line ").Replace("\\par", "\\line ");//似乎要在\\line后加一个空格
+		}
+
+		//将Tab键替换为指定数量的空格键
+		public string GetChangeTab2SpaceRtf(string oldRtf, int spaceOfTab)
+		{
+			string spaces = "";
+
+			for (int i = 0; i < spaceOfTab; i++)
+			{
+				spaces += " ";
+			}
+
+			return oldRtf.Replace("\\tab", spaces);
+		}
+
+		private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			if (cmbFontName.Text != "" && cmbFontSize.Text != "")
+			{
+				ChangeFont(cmbFontName.Text, Int32.Parse(cmbFontSize.Text));
+			}
+		}
+
+		private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			if (cmbFontName.Text != "" && cmbFontSize.Text != "")
+			{
+				ChangeFont(cmbFontName.Text, Int32.Parse(cmbFontSize.Text));
+			}
+		}
+
+		private void rtxCode_KeyDown(object sender, KeyEventArgs e)
+		{
+			if (ModifierKeys == Keys.Control && e.KeyCode == Keys.V)//粘贴操作
+			{
+				string rtf;
+
+				rtf = Clipboard.GetData(DataFormats.Rtf).ToString();
+				rtf = GetChangeEnter2ShfitEnterRtf(rtf);
+				if (cmbTabSpaces.SelectedIndex != 0)
+				{
+					rtf = GetChangeTab2SpaceRtf(rtf, Convert.ToInt32(cmbTabSpaces.Text));
+				}
+
+
+				rtxCode.Rtf = rtf;
+				ChangeFont(cmbFontName.Text, Int32.Parse(cmbFontSize.Text));
+
+				Clipboard.SetData(DataFormats.Rtf, rtxCode.Rtf.Trim());
+			}
+			else if (ModifierKeys == Keys.Control && e.KeyCode == Keys.C) //复制操作
+			{
+				Clipboard.SetData(DataFormats.Rtf, rtxCode.SelectedRtf);//将选中部分的RTF代码发送到剪贴板
+			}
+
+			e.SuppressKeyPress = true;//取消系统的按键命令
+		}
+
+		private void checkBox1_CheckedChanged(object sender, EventArgs e)
+		{
+			if (chkAlwaysOnTop.Checked)
+			{
+				TopMost = true;
+			}
+			else
+			{
+				TopMost = false;
+			}
+		}
+
+		private void btnHelp_Click(object sender, EventArgs e)
+		{
+			string info = "";
+
+			info += "说明：\n\n";
+			info += "    1. 本程序仅用于将代码编辑器中已格式化好的代码转换为适合放置在PPT中的格式（主要是将每行的段落回车Enter转换为换行回车Shift+Enter），本身并没有代码格式化功能。\n\n";
+			info += "    2. 在代码编辑器中选中已格式化好的代码，并按Ctrl+V粘贴到文本框中即可。进行粘贴操作后会自动将全部已转换好格式的代码放入剪贴板，此时只要在PPT中通过“保留源格式”的方式粘贴即可。也可在文本框中选中需要的部分，并按Ctrl+C复制到剪贴板，再粘贴到PPT中。\n\n";
+			info += "    3. 可以通过下拉菜单改变字体、字号与Tab键替换为的空格数（也可以不替换）。字体与字号可以实时改变，但空格数暂时需要在粘贴源代码到本程序前进行设置，不支持实时改变。\n\n";
+			info += "    4. 粘贴后的代码的字号，似乎取决于PPT主题的设置，字号设置暂时只影响在文本框中的外观，但仍暂时保留此功能。\n\n";
+			info += "    仓促之作，很不完善。若有好的建议，可联系作者邮箱 liuwei@nbu.edu.cn 。";
+
+			MessageBox.Show(info);
+		}
+
+
+
+	}
+}
